3ab06485a73a4ef2af1061196832e98e01777d88
Sam Staz <samstazi@iu.edu>
2023-10-11 23:51:18 -0400

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -44,4 +45,26 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:2.4.1")
+
+    // RecyclerView for displaying notes list
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+
+    // Navigation Component
+    implementation("androidx.navigation:navigation-fragment-ktx:2.4.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.4.1")
+
+    // ViewModel & LiveData
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+
+    // Room database
+    implementation("androidx.room:room-runtime:2.4.1")
+    // For Room's annotations processing
+    kapt("androidx.room:room-compiler:2.4.1")
+
+    // Fragment KTX for fragment operations
+    implementation("androidx.fragment:fragment-ktx:1.4.1")
 }
--- app/src/main/java/com/example/notetakingapplication/MainActivity.kt
+++ app/src/main/java/com/example/notetakingapplication/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.notetakingapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set up the navigation controller
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Set up the ActionBar with the NavController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    // This ensures that pressing the back button navigates through the navigation graph.
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
